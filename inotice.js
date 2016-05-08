0f9daa2f2d4df90e951982b3e6839035ac57ebe5
asari-koto <asari-koto@kayac.com>
2016-05-08 19:30:00 +0900

    jsからDomを作成するように

--- inotice.js
+++ inotice.js
@@ -7,54 +7,88 @@ var INotice = new function() {
   };
 
   var isActiveFlag = false;
+  var $bar, $time, $systemName, $notification, $icon;
 
-  var $bar = document.getElementsByClassName('ios-notification-bar-wrapper')[0];
-  var $time = document.querySelector('.message>.row1>.time');
-  var $systemName = document.querySelector('.message>.row1>.systemname');
-  var $notification = document.querySelector('.message>.row2>.notification');
-  var $icon = document.querySelector('.main-wrapper>.icon>img');
+  function init() {
+    appendINoticeDom();
 
-  this.init = function() {
-    this.setSystem(defaultConfig.systemName, defaultConfig.imageUrl);
-    this.setTime(defaultConfig.time);
-    this.setNotification(defaultConfig.notification);
+    $bar = document.getElementsByClassName('ios-notification-bar-wrapper')[0];
+    $time = document.querySelector('.message>.row1>.time');
+    $systemName = document.querySelector('.message>.row1>.systemname');
+    $notification = document.querySelector('.message>.row2>.notification');
+    $icon = document.querySelector('.main-wrapper>.icon>img');
   }
 
-  this.start = function() {
-      if (!isActiveFlag) {
-          this.open();
-          setTimeout( this.close, 2000 );
-      }
+  function appendINoticeDom() {
+    var inotice = document.createElement("div");
+    inotice.className = "ios-notification-bar-wrapper";
+
+    var heredoc = (function () {/*
+    <div class="ios-notification-bar">
+      <div class="main-wrapper">
+        <div class="icon">
+          <img src="">
+        </div>
+        <div class="message-wrapper">
+          <div class="message">
+            <div class="row1">
+              <p class="systemname"></p>
+              <p class="time"></p>
+            </div>
+            <div class="row2">
+              <p class="notification"></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="hole"></div>
+    </div>
+    */}).toString().match(/[^]*\/\*([^]*)\*\/\}$/)[1];
+
+    inotice.innerHTML = heredoc;
+    document.body.appendChild(inotice);
   }
 
-  this.open = function() {
-      $bar.className = 'ios-notification-bar-wrapper is-active';
-      isActiveFlag = true;
+  this.start = function(config) {
+    var cfg = config || defaultConfig;
+    setSystem(cfg.systemName, cfg.imageUrl);
+    setTime(cfg.time);
+    setNotification(cfg.notification);
+
+    if (!isActiveFlag) {
+      open();
+      setTimeout( close, 2000 );
+    }
   }
 
-  this.close = function() {
-      $bar.className = 'ios-notification-bar-wrapper';
-      setTimeout( function() {
-          isActiveFlag = false;
-      }, 500 );
+  function open() {
+    $bar.className = 'ios-notification-bar-wrapper is-active';
+    isActiveFlag = true;
   }
 
-  this.setSystem = function(sysName, imgPath) {
+  function close() {
+    $bar.className = 'ios-notification-bar-wrapper';
+    setTimeout( function() {
+      isActiveFlag = false;
+    }, 500 );
+  }
+
+  function setSystem(sysName, imgPath) {
     $systemName.textContent = sysName;
     $icon.src = imgPath;
   }
 
-  this.setTime = function(str) {
+  function setTime(str) {
     $time.textContent = str;
   }
 
-  this.setNotification = function(str) {
+  function setNotification(str) {
     $notification.textContent = str;
   }
 
   this.isActive = function() {
       return isActiveFlag;
   }
-}
 
-INotice.init();
+  document.addEventListener( 'DOMContentLoaded', init );
+}
